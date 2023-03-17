69d1fe73a6c9731ad6bf1b1559e9f6d636a2897a
girlkolawole <idiasgloria@gmail.com>
2023-03-17 01:06:33 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main (void)
+*main - Entry point
+*/
+int main(void)
 {
-	puts ("/"Programming is like building a multilingual puzzle);
-	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
