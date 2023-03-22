8bd57e6cccf21ef97c8b0d613d3785a3036a338d
girlkolawole <idiasgloria@gmail.com>
2023-03-22 16:09:45 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print function - print a function that prints in small alphabets
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= '2'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
