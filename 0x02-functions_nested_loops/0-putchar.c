0d935b719d70741cc6d911ef0bb815939151d075
girlkolawole <idiasgloria@gmail.com>
2023-03-22 10:45:09 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * main - print _putchar
- * written by kolawole
-* Return: 0
-*/
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
